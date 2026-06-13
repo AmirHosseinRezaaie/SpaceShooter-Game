f58ee46a07ad6fa9f73b0c54c862a12761a7e28c
amirhosein85 <amhs5831@gmail.com>
2026-06-13 12:24:24 +0330

    Implement abstract GameObject base class with movement and collision bounds

--- Final_Ap_Project/Final_Ap_Project/Models/GameObject.cs
+++ Final_Ap_Project/Final_Ap_Project/Models/GameObject.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Drawing;
 
 namespace Final_Ap_Project.Models
 {
-    internal class GameObject
+    public abstract class GameObject
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Speed { get; set; }
+        public Image ObjectImage { get; set; }
+
+        public GameObject(int x, int y, int width, int height, int speed, Image image)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Speed = speed;
+            ObjectImage = image;
+        }
+        public virtual void Draw(Graphics g)
+        {
+            if (ObjectImage != null)
+            {
+                g.DrawImage(ObjectImage, X, Y, Width, Height);
+            }
+        }
+        public abstract void Move();
+        public Rectangle GetBounds()
+        {
+            return new Rectangle(X, Y, Width, Height);
+        }
     }
 }
