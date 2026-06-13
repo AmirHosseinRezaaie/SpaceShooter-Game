66c7a5ccbcc0b22414891a1eacd1d2f545d96163
amirhosein85 <amhs5831@gmail.com>
2026-06-13 13:49:44 +0330

    Fixed bad form in program,cs

--- Final_Ap_Project/Final_Ap_Project/Program.cs
+++ Final_Ap_Project/Final_Ap_Project/Program.cs
@@ -1,3 +1,5 @@
+using Final_Ap_Project.UI;
+
 namespace Final_Ap_Project
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Final_Ap_Project
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainMenuForm());
         }
     }
 }
