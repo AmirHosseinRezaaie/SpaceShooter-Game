ff4d594bdaa45d71ac8c25a30afaef51572054f4
amirhosein85 <amhs5831@gmail.com>
2026-06-13 13:24:06 +0330

    Add Bullet class with multi-directional movement support

--- Final_Ap_Project/Final_Ap_Project/Models/Bullet.cs
+++ Final_Ap_Project/Final_Ap_Project/Models/Bullet.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Final_Ap_Project.Models;
+using System.Drawing;
 
 namespace Final_Ap_Project.Models
 {
-    internal class Bullet
+    public class Bullet : GameObject
     {
+        public bool IsPlayerBullet { get; set; }
+        public int SpeedX { get; set; }
+        public int SpeedY { get; set; }
+
+        public Bullet(int x, int y, int width, int height, int speedX, int speedY, Image image, bool isPlayerBullet)
+            : base(x, y, width, height, 0, image)
+        {
+            SpeedX = speedX;
+            SpeedY = speedY;
+            IsPlayerBullet = isPlayerBullet;
+        }
+
+        public override void Move()
+        {
+            X += SpeedX;
+            Y += SpeedY;
+        }
     }
 }
