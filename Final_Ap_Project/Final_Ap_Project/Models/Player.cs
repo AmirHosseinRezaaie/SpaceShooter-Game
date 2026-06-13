d01fd00650de5918ea2f49d2e9dc774fa710cc06
amirhosein85 <amhs5831@gmail.com>
2026-06-13 12:43:37 +0330

    Add Player class with HP and fire rate management

--- Final_Ap_Project/Final_Ap_Project/Models/Player.cs
+++ Final_Ap_Project/Final_Ap_Project/Models/Player.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Final_Ap_Project.Models;
+using System;
+using System.Drawing;
 
 namespace Final_Ap_Project.Models
 {
-    internal class Player
+    public class Player : GameObject
     {
+        public int HP { get; set; }
+        public int FireRateDelay { get; set; }
+        public long LastFireTime { get; set; }
+
+        public Player(int x, int y, int width, int height, int speed, Image image, int hp)
+            : base(x, y, width, height, speed, image)
+        {
+            HP = hp;
+            FireRateDelay = 200;
+            LastFireTime = 0;
+        }
+        public override void Move()
+        {
+
+        }
+        public override void Draw(Graphics g)
+        {
+            base.Draw(g);
+
+
+
+        }
     }
 }
